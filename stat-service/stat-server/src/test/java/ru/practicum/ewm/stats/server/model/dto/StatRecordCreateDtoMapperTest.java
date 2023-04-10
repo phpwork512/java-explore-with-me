0e19ab2af3f94c2a54451d55be948883bf20ab8e
phpwork512 <phpwork512@yandex.ru>
2023-04-10 18:10:50 +0400

    ewm stat server #3

--- stat-service/stat-server/src/test/java/ru/practicum/ewm/stats/server/model/dto/StatRecordCreateDtoMapperTest.java
+++ stat-service/stat-server/src/test/java/ru/practicum/ewm/stats/server/model/dto/StatRecordCreateDtoMapperTest.java
@@ -14,19 +14,19 @@ class StatRecordCreateDtoMapperTest {
 
     @Test
     void toStatRecord() {
-        StatRecordCreateDto statRecordCreateDto = StatRecordCreateDto.builder().
-                app("test-app").
-                uri("/test/uri").
-                ip("1.1.1.1").
-                timestamp(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")))
+        StatRecordCreateDto statRecordCreateDto = StatRecordCreateDto.builder()
+                .app("test-app")
+                .uri("/test/uri")
+                .ip("1.1.1.1")
+                .timestamp(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")))
                 .build();
 
-        StatRecord statRecord = StatRecord.builder().
-                appName("test-app").
-                uri("/test/uri").
-                ip("1.1.1.1").
-                timestamp(LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS)).
-                build();
+        StatRecord statRecord = StatRecord.builder()
+                .appName("test-app")
+                .uri("/test/uri")
+                .ip("1.1.1.1")
+                .timestamp(LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS))
+                .build();
 
         StatRecord mappedStatRecord = StatRecordCreateDtoMapper.toStatRecord(statRecordCreateDto);
         assertEquals(statRecord, mappedStatRecord);
