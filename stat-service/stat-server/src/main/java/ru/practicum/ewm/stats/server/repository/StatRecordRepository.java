c6439042dee9dfb9f8ef3ad1c1ac07571178fe74
phpwork512 <phpwork512@yandex.ru>
2023-04-10 18:21:49 +0400

    ewm stat server #4

--- stat-service/stat-server/src/main/java/ru/practicum/ewm/stats/server/repository/StatRecordRepository.java
+++ stat-service/stat-server/src/main/java/ru/practicum/ewm/stats/server/repository/StatRecordRepository.java
@@ -13,24 +13,28 @@ public interface StatRecordRepository extends JpaRepository<StatRecord, Long> {
     @Query("SELECT new ru.practicum.ewm.stats.common_dto.ViewStatsDto(s.appName, s.uri, COUNT(s.ip)) " +
             "FROM StatRecord s " +
             "WHERE s.timestamp >= ?1 AND s.timestamp < ?2 " +
-            "GROUP BY s.appName, s.uri")
+            "GROUP BY s.appName, s.uri " +
+            "ORDER BY COUNT(s.ip) DESC")
     List<ViewStatsDto> findStats(LocalDateTime start, LocalDateTime end);
 
     @Query("SELECT new ru.practicum.ewm.stats.common_dto.ViewStatsDto(s.appName, s.uri, COUNT(DISTINCT s.ip)) " +
             "FROM StatRecord s " +
             "WHERE s.timestamp >= ?1 AND s.timestamp < ?2 " +
-            "GROUP BY s.appName, s.uri")
+            "GROUP BY s.appName, s.uri " +
+            "ORDER BY COUNT(DISTINCT s.ip) DESC")
     List<ViewStatsDto> findStatsUniqueIp(LocalDateTime start, LocalDateTime end);
 
     @Query("SELECT new ru.practicum.ewm.stats.common_dto.ViewStatsDto(s.appName, s.uri, COUNT(s.ip)) " +
             "FROM StatRecord s " +
             "WHERE s.timestamp >= ?1 AND s.timestamp < ?2 AND s.uri IN ?3 " +
-            "GROUP BY s.appName, s.uri")
+            "GROUP BY s.appName, s.uri " +
+            "ORDER BY COUNT(s.ip) DESC")
     List<ViewStatsDto> findStatsForUriList(LocalDateTime start, LocalDateTime end, List<String> uris);
 
     @Query("SELECT new ru.practicum.ewm.stats.common_dto.ViewStatsDto(s.appName, s.uri, COUNT(DISTINCT s.ip)) " +
             "FROM StatRecord s " +
             "WHERE s.timestamp >= ?1 AND s.timestamp < ?2 AND s.uri IN ?3 " +
-            "GROUP BY s.appName, s.uri")
+            "GROUP BY s.appName, s.uri " +
+            "ORDER BY COUNT(DISTINCT s.ip) DESC")
     List<ViewStatsDto> findStatsForUriListAndUniqueIp(LocalDateTime start, LocalDateTime end, List<String> uris);
 }
