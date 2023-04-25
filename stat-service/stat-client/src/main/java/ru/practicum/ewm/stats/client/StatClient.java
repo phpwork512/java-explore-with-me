add34e93f2953b79f78e5ca9756a111208233514
phpwork512 <phpwork512@yandex.ru>
2023-04-25 20:30:37 +0400

    ewm main service #3

--- stat-service/stat-client/src/main/java/ru/practicum/ewm/stats/client/StatClient.java
+++ stat-service/stat-client/src/main/java/ru/practicum/ewm/stats/client/StatClient.java
@@ -7,7 +7,6 @@ import org.apache.http.HttpHeaders;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 import ru.practicum.ewm.stats.common_dto.StatRecordCreateDto;
 import ru.practicum.ewm.stats.common_dto.ViewStatsDto;
 
